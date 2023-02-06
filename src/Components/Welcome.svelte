5eb442a60b1cf55dbf91461164d16b1a96403e08
Eliel <Eliel@gmail.com>
2023-02-06 23:16:01 +0200

    initial commit

---
+++ src/Components/Welcome.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	import { expoOut } from 'svelte/easing';
+</script>
+
+<div id="welcome">
+	<h1 in:fly={{ y: -25, duration: 500, easing: expoOut, delay: 250 }}>Welcome to</h1>
+	<h1 in:fly={{ x: 150, duration: 750, easing: expoOut, delay: 750 }}>Agnon</h1>
+	<h1 in:fly={{ x: -150, duration: 750, easing: expoOut, delay: 1000 }}>Energy</h1>
+	<h1 in:fly={{ x: 150, duration: 750, easing: expoOut, delay: 1250 }}>Team</h1>
+</div>
+
+<style lang="scss">
+	#welcome {
+		display: flex;
+		height: 100vh;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		:first-child {
+			margin: 1rem 0;
+		}
+		h1 {
+			font-size: 3rem;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import IntersectionObserver from './../Components/IntersectionObserver.svelte';
+	import Welcome from './../Components/Welcome.svelte';
+  import "../Styling/global.scss"
+</script>
+
+<IntersectionObserver steps={100} threshold={50} let:visibleOnce>
+  {#if visibleOnce}
+    <Welcome />
+  {/if}
+</IntersectionObserver>
